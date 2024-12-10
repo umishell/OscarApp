2a9d2990a29f1799551154357a6df0d3b6ded923
umishell <160541720+umishell@users.noreply.github.com>
2024-12-09 22:27:20 -0300

    to finish  jwt verify

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import fastify from 'fastify';
-import fastifyPostgres from '@fastify/postgres';
+import fp from '@fastify/postgres';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
 const fast = fastify();
 const secret = process.env.SECRET_KEY; 
-const noAuthRoutes = ['/health','/login'];
+const noAuthRoutes = ['/health','/login','/filmes','/diretores'];
+
+
+fast.register(fp, {
+  connectionString: process.env.DATABASE_URL,
+});
+
 
 // global error handler
 fast.setErrorHandler((error, request, reply) => {
@@ -14,36 +20,13 @@ fast.setErrorHandler((error, request, reply) => {
 });
 
 
-fast.register(fastifyPostgres, {
-  connectionString: process.env.DATABASE_URL,
-}); 
+//// ASYNC FUNTIONS
 
 
-async function authenticate(request, reply) {
-
-  if (noAuthRoutes.includes(request.url)) {
-    return; // Skip authentication for noAuthRoutes
-  }
-  
-  try {
-    const token = request.headers.authorization;
-    jwt.verify(token, secret, (error, decoded) => {
-      if (error) {
-        throw new Error('Invalid token');
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    reply.code(401).send({ error: 'Unauthorized' });
-  }
-};
-
-fast.addHook('preHandler', authenticate);
 
 async function processVote(userId, movieId, directorId){
   try {
-    const client = await fast.pg.connect();
-    const result = await client.query('INSERT INTO votos (usuario_id, filme_id, diretor_id) VALUES ($1, $2, $3)', [userId, movieId, directorId]);
+    const result = await fast.pg.query('INSERT INTO votos (usuario_id, filme_id, diretor_id) VALUES ($1, $2, $3)', [userId, movieId, directorId]);
 
     if (result.rowCount === 0) {
       throw new Error('Failed to cast vote');
@@ -62,57 +45,100 @@ async function processVote(userId, movieId, directorId){
 //// ROUTES
 
 fast.get('/health', async (request, reply) => {
-  reply.send({ status: 'OK' });
+  reply.send({ status: 'SERVER RUNNING' });
 });
 
 
 
 fast.post('/login', async (request, reply) => {
-
-
   try {
     const { username, password } = request.body;
-
-    // Verify the user's credentials
-    const client = await fast.pg.connect();
-    const { rows } = await client.query('SELECT * FROM oscar.usuarios WHERE username = $1', [username]);
+    const { rows } = await fast.pg.query('SELECT * FROM oscar.usuarios WHERE username = $1', [username]);
 
     if (!rows || rows.length === 0) {
       throw new Error('Invalid username or password');
     }
     const user = rows[0];
+    const id = user.id;
 
     if (!user || user.password !== password) {
       throw new Error('Invalid username or password');
     }
 
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1h' });
+    // Generate TOKEN
+    const token = jwt.sign({ userId: user.id }, "secret", { expiresIn: '1h' });
 
-reply.send(token);
-
-    // Save the token to the tokens table
-    const result = await client.query('INSERT INTO oscar.tokens (token, usuario_id) VALUES ($1, $2) RETURNING issued_at', [token, user.id]);
+    const result = await fast.pg.query('INSERT INTO oscar.tokens (token, usuario_id) VALUES ($1, $2) RETURNING issued_at', [token, user.id]);
     const issuedAt = result.rows[0].issued_at;
-    reply.send({ token, issuedAt });
+
+    reply.send({ id, token, issuedAt });
 
   } catch (error) {
     console.error(error);
-    reply.code(500).send({ error: 'Internal Server Error' });
-  } finally {
-    // Release the connection back to the pool (important)
-    await client.release();
+    reply.code(500).send({ error: 'Problem logging in' });
   }
 });
 
 
 
+fast.get('/filmes', async (request, reply) => {
+  try {
+    const { rows } = await fast.pg.query('SELECT * FROM oscar.filmes');
+    reply.send(rows);
+  } catch (error) {
+    console.error('Error executing query:', error.message);
+    reply.code(500).send({ error: 'Problem getting movies data' });
+  }
+});
+
+
+
+fast.get('/diretores', async (request, reply) => {
+  try {
+    const { rows } = await fast.pg.query('SELECT * FROM oscar.diretores');
+    reply.send(rows);
+  } catch (error) {
+    console.error('Error executing query:', error.message);
+    reply.code(500).send({ error: 'Problem getting directors data' });
+  }
+});
+
+
+async function authenticate(request, reply) {
+  if (noAuthRoutes.includes(request.url)) {
+    return; // Skip authentication for noAuthRoutes
+  }
+
+  try {
+
+    const token = request.headers.authorization;
+    
+    const token2 = jwt.sign({ userId: 1 }, secret, { expiresIn: '1h' });
+    
+    
+    const decoded = await jwt.verify(token, "secret");
+    reply.send(decoded);
+    
+    // Check if the token has expired
+    if (decoded.exp < Date.now() / 1000) {
+      throw new Error('Token has expired');
+    }
+
+    request.body.userId = decoded.userId;
+
+  } catch (error) {
+    console.error(error);
+    reply.code(401).send({ error: 'Unauthorized' });
+  }
+};
+fast.addHook('preHandler', authenticate);
+
 fast.post('/vote', async (request, reply) => {
   try {
-      const userId = decoded.userId;
+      const userId = request.body.userId;
       const movieId = request.body.movieId;
       const directorId = request.body.directorId;
-
+reply.send(userId, movieId, directorId);
       processVote(userId, movieId, directorId)
         .then(() => {
           reply.send({ message: 'Vote successfully cast' });
@@ -124,37 +150,11 @@ fast.post('/vote', async (request, reply) => {
     
   } catch (error) {
     console.error(error);
-    reply.code(500).send({ error: 'Internal Server Error' });
+    reply.code(500).send({ error: 'Problem casting vote' });
   }
 });
 
 
-
-fast.get('/filmes', async (request, reply) => {
-  const client = await fast.pg.connect(); // Use fast.pg to connect
-
-  try {
-    const { rows } = await client.query('SELECT * FROM oscar.filmes');
-    reply.send(rows);
-
-  } finally {
-    await client.release();
-  }
-});
-
-
-
-fast.get('/diretores', async (request, reply) => {
-  const client = await fast.pg.connect(); // Use fast.pg to connect
-
-  try {
-    const { rows } = await client.query('SELECT * FROM oscar.diretores');
-    reply.send(rows);
-
-  } finally {
-    await client.release();
-  }
-});
 
 // ... (outras rotas)
 
